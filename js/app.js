5b61bbb5c9f3526de752ffeee51e5b8daa64fe69
roopakburman <roopak.burman@gmail.com>
2015-06-23 13:24:17 -0400

    updated code for SPA view

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
+angular.module('starter', ['ionic', 'ngRoute', 'starter.home', 'starter.menu', 'starter.settingsPage', 'starter.register', 'starter.follow'])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/starter', {
+    templateUrl: 'home.html',
+    controller: 'homeCtrl'
+  });
+$routeProvider.otherwise({redirectTo: '/register'});
+  
+}]);
+/* .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs).
@@ -22,4 +29,6 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-});
+}); */
+
+
